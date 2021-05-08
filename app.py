643c3a084646fac1fbc1d010950bc829a5f3bcbb
pratadaniel94 <danielprata@outlook.com>
2021-05-08 06:43:05 -0300

    logic adjustment

--- app.py
+++ app.py
@@ -68,54 +68,67 @@ class Game:
                 return player
 
     def zerar_jogador(self, nome):
+        print(f'perdeu: {nome}')
         for propriedade in self.tabuleiro:
             if nome == propriedade.proprietario:
                 propriedade.proprietario = None
         self.get_player(nome).desativar()
+        if self.check_vencedor():
+            raise KeyboardInterrupt
+
+    def check_vencedor(self):
+        player_on = []
+        for player in self.jogadores:
+            if player.status:
+                player_on.append(player)
+        if len(player_on) == 1:
+            return player_on[0]
+
 
 
     def start(self):
-        while self.rodadas < 1000:
-            self.rodadas += 1
-            print(f'Rodada {self.rodadas}', end="\n\n")
-            for player in self.jogadores:
-                if player.status:
-                    posicao = self.tabuleiro[player.posicao-1].nome if player.posicao else None
-                    print(f'{player.nome} posição: {posicao} saldo: {player.saldo}')
-                    numero = Dado.get_numero()
-                    print('numero sortido', numero)
-                    player.posicao += numero
+        while self.rodadas < 100:
+            try:
+                self.rodadas += 1
+                print(f'Rodada {self.rodadas}', end="\n\n")
+                for player in self.jogadores:
+                    if player.status:
+                        posicao = self.tabuleiro[player.posicao-1].nome if player.posicao else None
+                        print(f'{player.nome} posição: {posicao} saldo: {player.saldo}')
+                        numero = Dado.get_numero()
+                        print('numero sortido', numero)
+                        player.posicao += numero
 
-                    #Verifica se chegou final do tabuleiro
-                    if player.posicao > self.qtd_casas:
-                        player.posicao -= self.qtd_casas
-                        player.saldo += 100
+                        #Verifica se chegou final do tabuleiro
+                        if player.posicao > self.qtd_casas:
+                            player.posicao -= self.qtd_casas
+                            player.saldo += 100
 
-                    propriedade = self.tabuleiro[player.posicao-1]
-                    print(f'propriedade {propriedade.nome}, dono: {propriedade.proprietario}')
+                        propriedade = self.tabuleiro[player.posicao-1]
+                        print(f'propriedade {propriedade.nome}, dono: {propriedade.proprietario}')
 
-                    #Compra do imovel de acordo com perfil
-                    if not propriedade.proprietario:
-                        if player.saldo >= propriedade.custo_venda:
-                            propriedade.proprietario = player.nome
-                            player.saldo -= propriedade.custo_venda
+                        #Compra do imovel de acordo com perfil
+                        if not propriedade.proprietario:
+                            if player.saldo >= propriedade.custo_venda:
+                                propriedade.proprietario = player.nome
+                                player.saldo -= propriedade.custo_venda
 
-                    #Paga aluguel caso o imovel pertence a outro jogador
-                    elif propriedade.proprietario != player.nome:
-                        if player.saldo >= propriedade.aluguel:
-                            recebedor = self.get_player(propriedade.proprietario)
-                            if propriedade.proprietario == recebedor.nome:
-                                player.pagar(propriedade.aluguel, recebedor)
-                        #Jogador Eliminado
-                        else:
-                            recebedor = self.get_player(propriedade.proprietario)
-                            player.pagar(player.saldo, recebedor)
-                            self.zerar_jogador(player.nome)
+                        #Paga aluguel caso o imovel pertence a outro jogador
+                        elif propriedade.proprietario != player.nome:
+                            if player.saldo > propriedade.aluguel:
+                                recebedor = self.get_player(propriedade.proprietario)
+                                if propriedade.proprietario == recebedor.nome:
+                                    player.pagar(propriedade.aluguel, recebedor)
+                            #Jogador Eliminado
+                            else:
+                                recebedor = self.get_player(propriedade.proprietario)
+                                player.pagar(player.saldo, recebedor)
+                                self.zerar_jogador(player.nome)
+                    print()
+            except KeyboardInterrupt:
+                print('Game winner')
+                break
 
-
-
-
-                print()
         for player in self.jogadores:
             print(f'{player.nome}: {player.saldo}')
 
